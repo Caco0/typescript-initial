d2141fb3c16b70484e6ab725a57cb24db6d24e8d
Rafael Dametto <cacotpds@gmail.com>
2023-09-21 11:08:46 -0300

    retorno do Generics tipado

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 /**Generics */
 
-function concatArray(...itens: any[]): any[] {
+function concatArray<T>(...itens: T[]): T[] {
   return new Array().concat(...itens);
 }
-const numArray = concatArray([1, 3, 6], [8, 2]);
-
-const stringArray = concatArray(
+const numArray = concatArray<number[]>([1, 3, 6], [8, 2]); // o retorno da concatenação dos arrays é tipado pelo <T> e podendo receber ainda qualquer tipo de array
+const stringArray = concatArray<string[]>(
   ["Rafael"],
   ["Fortunato"],
   ["Dametto", "é um rapaz lindo, cheiroso e gostoso!!!"]
