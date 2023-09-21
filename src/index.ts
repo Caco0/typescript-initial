7fa34b62c8782d4981f42f073c39d3ae56fd68b0
Rafael Dametto <cacotpds@gmail.com>
2023-09-21 16:23:14 -0300

    Atribute Decorator

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,37 @@ function apiVersion(version: any) {
     Object.assign(target.prototype, { __version: version });
   };
 }
-@apiVersion("1.10")
-class Api {}
 
-const api = new Api();
-console.log(`Version: ${api.__version}`);
+function minLength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => "[Api]-> " + _value;
+    const setter = (value: string) => {
+      if (value.length < length) {
+        throw new Error(
+          `Nome da Api muito pequeno, ${length} é a quantidade mínima de caracteres!`
+        );
+      } else {
+        _value = value;
+      }
+    };
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+// Atribute Decorator
+
+class Api {
+  @minLength(5)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api = new Api("Usuário");
+console.log(api.name);
